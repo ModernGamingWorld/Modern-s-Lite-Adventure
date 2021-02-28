c4a96100deea3dd34ac9a2950c0436c610e4bad0
ModernGamingWorld <>
2021-02-28 16:42:22 -0600

    300+ Recipe additions

---
+++ kubejs/client_scripts/item_modifiers/jei_descriptions.js
@@ -0,0 +1,3 @@
+onEvent('jei.information', event => {
+  event.add(''astralsorcery:stardust'', ['Obtained by left-clicking a Starmetal Ingot in-world.'])  event.add(''astralsorcery:stardust'', ['Obtained by left-clicking a Starmetal Ingot in-world.'])
+})
